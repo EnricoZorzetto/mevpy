1dd2b58fbf64826c81015ea97d121982ee30f193
Enrico Zorzetto <34559183+EnricoZorzetto@users.noreply.github.com>
2018-01-04 18:25:56 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,14 @@ from setuptools import setup
 
 setup(
    name='mevpy',
-   version='1.00',
+   version='1.01',
    description='mevpy package',
    author='Enrico Zorzetto',
    author_email='enrico.zorzetto@duke.edu',
+   url = 'https://github.com/EnricoZorzetto/mevpy'
+   download_url = 'https://github.com/EnricoZorzetto/mevpy/archive/1.01.tar.gz'
    packages=['mevpy'],  #same as name
    install_requires=['matplotlib', 'pandas', 'numpy', 'scipy'], #external packages as dependencies
 )
+
+
