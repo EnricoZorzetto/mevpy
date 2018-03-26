cf7df644431c447487d059fc270f0745c3594404
Enrico Zorzetto <34559183+EnricoZorzetto@users.noreply.github.com>
2018-03-26 15:44:11 -0400

    Update setup.py
    updated to v1.0.4

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 
 setup(
    name='mevpy',
-   version='1.03',
+   version='v1.0.4',
    description='mevpy package',
    author='Enrico Zorzetto',
    author_email='enrico.zorzetto@duke.edu',
    url = 'https://github.com/EnricoZorzetto/mevpy',
-   download_url = 'https://github.com/EnricoZorzetto/mevpy/archive/1.03.tar.gz',
+   download_url = 'https://github.com/EnricoZorzetto/mevpy/archive/v1.0.4.tar.gz',
    packages=['mevpy'],  
    install_requires=['matplotlib', 'pandas', 'numpy', 'scipy'], 
 )
